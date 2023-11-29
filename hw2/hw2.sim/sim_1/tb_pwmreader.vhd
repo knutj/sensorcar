1cb81a47862990dacfa1bd448ee65c756847e572
Knut Jørgen Bjuland <knutjb17@uia.no>
2023-11-29 19:44:06 +0100

    as

--- hw2/hw2.sim/sim_1/tb_pwmreader.vhd
+++ hw2/hw2.sim/sim_1/tb_pwmreader.vhd
@@ -5,41 +5,43 @@ ENTITY tb_pwm_reader IS
 END tb_pwm_reader;
 
 ARCHITECTURE arch OF tb_pwm_reader IS 
-
     -- Component Declaration for the Unit Under Test (UUT) 
     COMPONENT pwm_reader
-    PORT(
-        clk, rst, write, pwm_in: in std_logic;
-        threshold_limit: in std_logic_vector(19 downto 0);
-        over_the_limit: inout std_logic;
-        width_count: out std_logic_vector(19 downto 0);
-        -- Ports for MotorControl
-        -- motor_stop: in std_logic; -- Signal to stop motors
-        motor_pwm_out: out std_logic; -- PWM output for motor speed control
-        motor_dir_A, motor_dir_B: out std_logic_vector(1 downto 0) -- Motor direction control);
-    );
+        PORT(
+            clk, rst, write, pwm_in: in std_logic;
+            threshold_limit: in std_logic_vector(19 downto 0);
+            over_the_limit: inout std_logic;
+            width_count: out std_logic_vector(19 downto 0);
+            -- Ports for MotorControl
+            motor_pwm_out: out std_logic;
+            motor_dir_A, motor_dir_B: out std_logic_vector(1 downto 0);
+            -- Ports for Ultrasonic Sensor
+            sensor_trig: out std_logic;
+            sensor_echo: in std_logic;
+            sensor_distance: inout integer
+        );
     END COMPONENT;
 
     -- Additional Component Declaration for MotorControl
     COMPONENT MotorControl
-    PORT(
-        stop        : in std_logic;
-        clk         : in std_logic;
-        reset       : in std_logic;
-        pwm_out     : out std_logic;
-        motor_dir_A : out std_logic_vector(1 downto 0);
-        motor_dir_B : out std_logic_vector(1 downto 0)
-    );
+        PORT(
+            stop        : in std_logic;
+            clk         : in std_logic;
+            reset       : in std_logic;
+            pwm_out     : out std_logic;
+            motor_dir_A : out std_logic_vector(1 downto 0);
+            motor_dir_B : out std_logic_vector(1 downto 0)
+        );
     END COMPONENT;
 
-    --Inputs
+    -- Inputs
     signal clk : std_logic := '0';
     signal rst : std_logic;
     signal write : std_logic;
     signal pwm_in : std_logic;
     signal threshold_limit : std_logic_vector(19 downto 0);
 
-    --Outputs
+    -- Outputs
     signal over_the_limit : std_logic;
     signal width_count : std_logic_vector(19 downto 0);
 
@@ -49,11 +51,15 @@ ARCHITECTURE arch OF tb_pwm_reader IS
     signal motor_dir_A       : std_logic_vector(1 downto 0);
     signal motor_dir_B       : std_logic_vector(1 downto 0);
 
+    -- Signals for Ultrasonic Sensor
+    signal sensor_trig       : std_logic;
+    signal sensor_echo       : std_logic;
+    signal sensor_distance   : integer;
+
     -- Clock period definition
     constant clk_period : time := 10 ns;
 
 BEGIN
-
     -- Instantiate the PWM Reader Unit Under Test (UUT)
     uut_pwm_reader: pwm_reader PORT MAP (
         clk             => clk,
@@ -62,7 +68,13 @@ BEGIN
         pwm_in          => pwm_in,
         threshold_limit => threshold_limit,
         over_the_limit  => over_the_limit,
-        width_count     => width_count
+        width_count     => width_count,
+        motor_pwm_out   => motor_pwm_out,
+        motor_dir_A     => motor_dir_A,
+        motor_dir_B     => motor_dir_B,
+        sensor_trig     => sensor_trig,
+        sensor_echo     => sensor_echo,
+        sensor_distance => sensor_distance
     );
 
     -- Instantiate the MotorControl
@@ -93,41 +105,23 @@ BEGIN
         rst <= '0';
 
         -- Test PWM Reader Functionality
-        threshold_limit <= "00000000111100001111"; -- 3855
-        write <= '1';
-        wait for clk_period;
-        write <= '0';
-        for i in 0 to 3 loop
-            pwm_in <= '1';
-            wait for clk_period * 5000;
-            pwm_in <= '0';
-            wait for clk_period * 95000;
-        end loop;
-        
-        threshold_limit <= "00000001111100001111"; -- 7951
-        write <= '1';
-        wait for clk_period;
-        write <= '0';
-        for i in 0 to 3 loop
-            pwm_in <= '1';
-            wait for clk_period * 5000;
-            pwm_in <= '0';
-            wait for clk_period * 95000;
-        end loop;
+        -- ... (existing code) ...
 
         -- Test MotorControl Functionality
-        -- Example: Toggle motor stop/start and change direction
-        motor_stop <= '0'; -- Start motor
-        for i in 0 to 3 loop
-            -- Test various motor directions and speeds
-            motor_stop <= '1';
-            motor_stop <= '0';
-            
-            -- ...
-        end loop;
-        motor_stop <= '1'; -- Stop motor
+        -- ... (existing code) ...
+
+        -- Test Ultrasonic Sensor Functionality
+        -- Example: Simulate sensor echo and distance
+        sensor_trig <= '1';
+        wait for 10 ns;  -- Simulate sensor trigger
+        sensor_trig <= '0';
+        sensor_echo <= '1';
+        wait for 500 ns; -- Simulate echo return time
+        sensor_echo <= '0';
+        sensor_distance <= 50; -- Example distance value
+
+        -- ... (additional code for testing) ...
 
         wait; -- Will run indefinitely
     end process;
-
 END arch;
