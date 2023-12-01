dd64bdf63eabe90005fe914c32683cb88a50ad7b
sonbit <sondre.bitubekk@gmail.com>
2023-12-01 16:05:48 +0100

    Update mod_m_counter.vhd

--- asip/asip.srcs/sources_1/new/mod_m_counter.vhd
+++ asip/asip.srcs/sources_1/new/mod_m_counter.vhd
@@ -1,23 +1,23 @@
 library ieee;
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;
+use work.constants_pkg.all;
 
 entity mod_m_counter is
     generic (
-        BITS_N  : integer := 9;
-        MOD_M   : integer := 326
+        M : integer := 326 -- Modulus M
     );
     port (
         clk         : in    std_logic;
         rst         : in    std_logic;
         max_tick    : out   std_logic;
-        mc_q        : out   std_logic_vector(BITS_N - 1 downto 0)
+        mc_q        : out   std_logic_vector(DR_DATA_WIDTH - 1 downto 0)
     );
 end mod_m_counter;
 
 architecture arch of mod_m_counter is
-    signal r_reg    : unsigned(BITS_N - 1 downto 0);
-    signal r_next   : unsigned(BITS_N - 1 downto 0);
+    signal r_reg    : unsigned(DR_DATA_WIDTH - 1 downto 0);
+    signal r_next   : unsigned(DR_DATA_WIDTH - 1 downto 0);
 begin
     process (clk, rst)
     begin
@@ -28,8 +28,8 @@ begin
         end if;
     end process;
 
-    r_next <= r_reg + 1 when r_reg < (MOD_M - 1) else (others => '0');
-    max_tick <= '0'     when r_reg < (MOD_M - 1) else '1';
-    mc_q <= std_logic_vector(r_reg);
+    r_next      <= r_reg + 1    when r_reg < (M - 1) else (others => '0');
+    max_tick    <= '0'          when r_reg < (M - 1) else '1';
+    mc_q        <= std_logic_vector(r_reg);
     
 end arch;
