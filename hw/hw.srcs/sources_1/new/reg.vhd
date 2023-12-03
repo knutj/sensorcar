1b4afb2d3e1b04540e7a28d7188e71cddc8ec478
sonbit <sondre.bitubekk@gmail.com>
2023-12-03 12:43:00 +0100

    Added initial structure of a hw approach

---
+++ hw/hw.srcs/sources_1/new/reg.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.constants_pkg.all;
+
+entity reg is
+    generic (
+        REG_WIDTH : integer
+    );
+    port ( 
+        clk     : in    std_logic;
+        rst     : in    std_logic;
+        reg_ld  : in    std_logic;
+        reg_d   : in    std_logic_vector(REG_WIDTH - 1 downto 0);
+        reg_q   : out   std_logic_vector(REG_WIDTH - 1 downto 0)
+    );
+end reg;
+
+architecture arch of reg is
+begin
+    process (clk, rst)
+    begin
+        if rst = '1' then
+            reg_q <= (others => '0');
+        elsif rising_edge(clk) and reg_ld = '1' then
+            reg_q <= reg_d;
+        end if;
+    end process;
+end arch;
---
+++ hw/hw.srcs/sources_1/new/up_counter.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.constants_pkg.all;
+
+entity up_counter is
+    port (
+        clk     : in    std_logic;
+        rst     : in    std_logic;
+        uc_clr  : in    std_logic;
+        uc_cnt  : in    std_logic;
+        uc_q    : out   std_logic_vector(THRESHOLD_WIDTH - 1 downto 0)
+    );
+end up_counter;
+
+architecture arch of up_counter is
+    signal r_reg : unsigned(THRESHOLD_WIDTH - 1 downto 0);
+    signal r_next: unsigned(THRESHOLD_WIDTH - 1 downto 0);
+begin
+    process (clk, rst)
+    begin
+        if rst = '1' then
+            r_reg <= (others => '0');
+        elsif rising_edge(clk) then
+            r_reg <= r_next;
+        end if;
+    end process;
+
+    r_next <=
+        r_reg + 1       when uc_cnt = '1' else
+        (others => '0') when uc_clr = '1' else
+        r_reg;
+    
+    uc_q <= std_logic_vector(r_reg);
+    
+end arch;
