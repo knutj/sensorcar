bec7a8ae6606314a0496aa5c96e89f02a704bc71
Knut Jørgen Bjuland <knutjb17@uia.no>
2023-12-04 09:23:45 +0100

    work somehow stops but no left turn ot revese

--- fmsd_car_source/hw3.srcs/sim_1/new/tb_top.vhd
+++ fmsd_car_source/hw3.srcs/sim_1/new/tb_top.vhd
@@ -49,37 +49,60 @@ BEGIN
     );
 
     -- Clock process definitions
-    clk_process : PROCESS
-    BEGIN
-        clk <= '0';
-        wait for clk_period/2;
-        clk <= '1';
-        wait for clk_period/2;
-    END PROCESS;
+    --clk_process : PROCESS
+    --BEGIN
+    --    clk <= '0';
+    --    wait for clk_period/2;
+     --   clk <= '1';
+     --   wait for clk_period/2;
+       
+    --END PROCESS;
 
     -- Test process
-    stim_proc: PROCESS
-    BEGIN
-        -- Initialize Inputs
+    stim_proc: PROCESS    BEGIN
+    -- Initialize Inputs
         rst <= '1';
-        wait for 100 ns;
+        wait for 10 ns;
         
         -- Reset the system
         rst <= '0';
         wait for 100 ns;
+        clk <= '1';
+        
 
         -- Simulate echo signal and other test conditions
+        -- State: idle -> start_motor
         echo <= '0';
         wait for 500 ns; -- Adjust the timing as needed
+
+        -- State: start_motor -> send_trigger
+        trigger <='1';
+        motor_pwm(0) <= '1';
+        motor_pwm(2) <= '1';
+        motor_pwm(4) <= '1';
+        motor_pwm(6) <= '1';
+        
+        wait for 10 ns;
+        
+
+        -- State: send_trigger -> check_echo
+        -- Simulate object detected
+        echo <= '1'; 
+        wait for 10 ns;
+
+        -- State: check_echo -> stop_motor
         echo <= '1';
-        wait for 100 ns; -- Adjust the timing as needed
-        echo <= '0';
-        wait for 400 ns; -- Adjust the timing as needed
-        -- ... Add more test cases as needed ...
-        trigger <= '1';
-        wait for 10 ms;
-        echo <= '1';
-        trigger  <= '1';
+        wait for 10 ns;
+
+        -- State: stop_motor -> reverse_motor
+        wait for 10 ns;
+
+        -- State: reverse_motor -> turn_left
+        wait for 10 ns;
+
+        -- State: turn_left -> idle
+        wait for 10 ns;
+
         wait for 30 ms;
         wait; -- Will run indefinitely
     END PROCESS;
