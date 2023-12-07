5f3be809b6d75e96fb8cd2c3b3cc190676971d3f
sonbit <sondre.bitubekk@gmail.com>
2023-12-07 10:51:32 +0100

    ASIP Instruction WIP

--- asip/asip.srcs/sources_1/new/top_echo.vhd
+++ asip/asip.srcs/sources_1/new/top_echo.vhd
@@ -3,10 +3,6 @@ use ieee.std_logic_1164.all;
 use work.constants_pkg.all;
 
 entity top_echo is
-     generic (
-        N : integer := 9;  -- Number of bits (E.g. 2^N > M)
-        M : integer := 501 -- Modulus M
-    );
     port (
         clk         : in    std_logic;
         rst         : in    std_logic;
@@ -15,9 +11,7 @@ entity top_echo is
         threshold   : in    std_logic_vector(PWM_WIDTH - 1 downto 0);
         above_limit : out   std_logic;
         width_count : out   std_logic_vector(PWM_WIDTH - 1 downto 0);
-        trig     : inout  std_logic;
-        max_tick    : inout   std_logic;
-        mc_q        : out   std_logic_vector(N - 1 downto 0)
+        trig        : out   std_logic
     );
 end top_echo;
 
@@ -28,16 +22,15 @@ architecture arch of top_echo is
     signal top_ucq  : std_logic_vector(PWM_WIDTH - 1 downto 0);
     signal top_hrq  : std_logic_vector(PWM_WIDTH - 1 downto 0);
     signal top_trq  : std_logic_vector(PWM_WIDTH - 1 downto 0);
+    
 begin
-
-
-    maxcounter : entity work.mod_m_counter(arch)
+    trig_counter : entity work.trig_counter(arch)
     port map (
-        clk => clk,
-        rst => rst,
-        max_tick => max_tick,
-        mc_q  => mc_q 
-        );
+        clk         => clk,
+        rst         => rst,
+        start       => '1',
+        trig        => trig
+    );
 
     up_counter : entity work.up_counter(arch)
     port map (
@@ -48,7 +41,8 @@ begin
         uc_q        => top_ucq
     );
     
-    hold_reg : entity work.reg_echo(arch)
+    hold_reg : entity work.reg(arch)
+    generic map (REG_WIDTH => PWM_WIDTH)
     port map (
         clk         => clk,
         rst         => rst,
@@ -57,7 +51,8 @@ begin
         reg_q       => top_hrq
     );
     
-    threshold_reg : entity work.reg_echo(arch)
+    threshold_reg : entity work.reg(arch)
+    generic map (REG_WIDTH => PWM_WIDTH)
     port map (
         clk         => clk,
         rst         => rst,
@@ -73,16 +68,11 @@ begin
         echo        => echo,
         clr         => top_clr,
         cnt         => top_cnt,
-        ld          => top_ld,
-        max_tick    => max_tick,
-        trig     => trig
+        ld          => top_ld
     );
     
     -- Comparator
     above_limit <= '0' when top_trq > top_hrq else '1';
-    
-    
-
     
     -- Width measurement
     width_count <= top_hrq;
