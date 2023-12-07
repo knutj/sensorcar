6273c0d9683d865f3dbe3bf7b427b17805566d3e
Knut Jørgen Bjuland <knutjb17@uia.no>
2023-12-07 14:50:30 +0100

    car moves add two new opcode to start car and to start sensor

--- asip_v2/asip_v2.srcs/sources_1/imports/OneCycleCPUwithIO_files/imem.vhd
+++ asip_v2/asip_v2.srcs/sources_1/imports/OneCycleCPUwithIO_files/imem.vhd
@@ -27,24 +27,23 @@ architecture arch of imem is
       -- These instructions are represented in hexadecimal format
       -- addr 00 to 31: Representing 32 instructions (or void spaces) in the memory
       -- Main:
-     x"060380",  -- addr 00:     LD   R7,6      # 00000110(imm) 000(rs2=  ) 000(rs1=  ) 111(rd=R7) 0000000 
-      x"001D01",  -- addr 01:     LD   R2,R7     # 00000000(imm) 000(rs2=  ) 111(rs1=R7) 010(rd=R2) 0000001 
-	  x"0A5C03",  -- addr 02:     ST   R2,10(R7) # 00001010(imm) 010(rs2=R2) 111(rs1=R7) 000(rd=  ) 0000011
-	  x"001F85",  -- addr 03:     INC  R7        # 00000000(imm) 000(rs2=  ) 111(rs1=R7) 111(rd=R7) 0000101
-	  x"00E886",  -- addr 04: L1: ADD  R1,R2,R7  # 00000000(imm) 111(rs2=R7) 010(rs1=R2) 001(rd=R1) 0000110
-	  x"002487",  -- addr 05:     SUB  R1,R1,R1  # 00000000(imm) 001(rs2=R1) 001(rs1=R1) 001(rd=R1) 0000111
-	  x"004888",  -- addr 06:     ORR  R1,R2,R2  # 00000000(imm) 010(rs2=R2) 010(rs1=R2) 001(rd=R1) 0001000
-	  x"FF0489",  -- addr 07:     ORI  R1,R1,xFF # 11111111(imm) 000(rs2=  ) 001(rs1=R1) 001(rd=R1) 0001001
-	  x"00448A",  -- addr 08:     ANR  R1,R1,R2  # 00000000(imm) 010(rs2=R2) 001(rs1=R1) 001(rd=R1) 0001010
-	  x"00088B",  -- addr 09:     ANI  R1,R2,x00 # 00000000(imm) 000(rs2=  ) 010(rs1=R2) 001(rd=R1) 0001011
-	  x"001F84",  -- addr 10:     DEC  R7        # 00000000(imm) 000(rs2=  ) 111(rs1=R7) 111(rd=R7) 0000100
-	  x"F91C0D",  -- addr 11:     JRNZ R7,L1     # 11111001(imm) 000(rs2=  ) 111(rs1=R7) 000(rd=  ) 0001101
-	  x"01080C",  -- addr 12:     JRZ  R7,L2     # 00000001(imm) 000(rs2=  ) 010(rs1=R7) 000(rd=  ) 0001100
-	  x"0A0B82",  -- addr 13: L2: LD   R7,10(R2) # 00001010(imm) 000(rs2=  ) 010(rs1=R2) 111(rd=R7) 0000010
-	  x"00E010",  -- addr 14:     ST   R7,OUT    # 00000000(imm) 111(rs2=R7) 000(rs1=  ) 000(rd=  ) 0010000
-	  x"00038F",  -- addr 15:     LD   R7,IN     # 00000000(imm) 000(rs2=  ) 000(rs1=  ) 111(rd=R7) 0001111
-	  x"00000E",  -- addr 16:     J    L2        # 00000000(imm) 000(rs2=  ) 000(rs1=  ) 000(rd=  ) 0001110	
-	  x"FFFFFF",  -- addr 17: (void)
+      x"000020", -- addr 01: New instruction with opcode 0100000
+      x"000040", -- addr 02: New instruction with opcode 1000000
+	  x"FFFFFF",  -- addr 03: (void)
+	  x"FFFFFF",  -- addr 04: (void)
+	  x"FFFFFF",  -- addr 05: (void)
+	  x"FFFFFF",  -- addr 06: (void)
+	  x"FFFFFF",  -- addr 07: (void)
+	  x"FFFFFF",  -- addr 08: (void)
+	  x"FFFFFF",  -- addr 09: (void)
+	  x"FFFFFF",  -- addr 10: (void)
+	  x"FFFFFF",  -- addr 11: (void)
+	  x"FFFFFF",  -- addr 12: (void)
+	  x"FFFFFF",  -- addr 13: (void)
+	  x"FFFFFF",  -- addr 14: (void)
+	  x"FFFFFF",  -- addr 15: (void)
+	  x"FFFFFF",  -- addr 16: (void)
+	  x"FFFFFF",   -- addr 17: (void) 
 	  x"FFFFFF",  -- addr 18: (void)
 	  x"FFFFFF",  -- addr 19: (void)
 	  x"FFFFFF",  -- addr 20: (void)
@@ -57,6 +56,7 @@ architecture arch of imem is
 	  x"FFFFFF",  -- addr 27: (void)
 	  x"FFFFFF",  -- addr 28: (void)
 	  x"FFFFFF",  -- addr 29: (void)
+	  x"FFFFFF",  -- addr 20: (void)
 	  x"FFFFFF",  -- addr 30: (void)
 	  x"FFFFFF"   -- addr 31: (void) 
    );
