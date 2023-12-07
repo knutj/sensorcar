44ac3338123a2a54619e53542b6ec29a73ba85e9
sonbit <32852496+sonbit@users.noreply.github.com>
2023-12-07 10:54:55 +0100

    Delete sw_car/platform_car directory

--- sw_car/platform_car/zynq_fsbl/build/CMakeFiles/fsbl.elf.dir/ps7_init.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fsbl.elf.dir/ps7_init.c.obj: \
- /home/knutjb/Vivado/sw_car/platform_car/zynq_fsbl/ps7_init.c \
- /home/knutjb/Vivado/sw_car/platform_car/zynq_fsbl/ps7_init.h
-/home/knutjb/Vivado/sw_car/platform_car/zynq_fsbl/ps7_init.h:
--- sw_car/platform_car/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/libsrc/standalone/src/CMakeFiles/xilstandalone.dir/common/xil_testcache.c.obj.d
+++
@@ -1,2 +0,0 @@
-libsrc/standalone/src/CMakeFiles/xilstandalone.dir/common/xil_testcache.c.obj: \
- /home/knutjb/Vivado/sw_car/platform_car/zynq_fsbl/zynq_fsbl_bsp/libsrc/standalone/src/common/xil_testcache.c
